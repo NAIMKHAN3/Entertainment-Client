bf6ec8123367c2c08ad74c6dd2a29f83101592e5
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-10-14 04:07:06 +0600

    initial Setup Done

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   <div>
+    <h1 className='text-4xl font-semibold text-red-600'>Entertainment movie website</h1>
+   </div>
+  )
+}
---
+++ src/redux/RootApi/apiSlice.ts
@@ -0,0 +1,12 @@
+import { axiosBaseQuery } from '@/axios/axios'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// Define a service using a base URL and expected endpoints
+export const apiSlice = createApi({
+  reducerPath: 'apiSlice',
+  baseQuery: axiosBaseQuery({ baseUrl: 'https://entertainment-movie-qrvqu4md1-naimkhan3.vercel.app/api/v1' }),
+  endpoints: () => ({}),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
