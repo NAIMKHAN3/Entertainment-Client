bb24c27d0560da9cc1cc36167d04392910c97c7b
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-10-14 04:49:03 +0600

    sign in page added

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -12,7 +12,7 @@ type Inputs = {
   email: string
   address: string
 }
-const Signup = () => {
+const Signin = () => {
   const { control, register, handleSubmit, watch, formState: { errors }, setValue } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     console.log(data)
@@ -24,19 +24,19 @@ const Signup = () => {
     }
   }
   return (
-    <div className='w-full md:w-2/3 lg:w-1/3 mx-auto border p-5 mt-10 rounded-md'>
-      <h1 className='text-center font-semibold text-2xl'>Sign Up</h1>
+    <div className='w-full md:w-2/3 lg:w-1/3 mx-auto border p-5 mt-20 rounded-md'>
+      <h1 className='text-center font-semibold text-2xl'>Sign In</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <FormInput type='text' name='name' lebel='Your Name' placeholder='name' />
-        <FormInput type='text' name='contactNo' lebel='Your Contact No' placeholder='contact no' />
+        
+        
         <FormInput type='email' name='email' lebel='Your Email' placeholder='email' />
         <FormInput type='password' name='password' lebel='Your password' placeholder='password' />
-        <FormInput type='text' name='address' lebel='Your Address' placeholder='address' />
-        <h1 className='text-center'>Already have an account please <span className='text-blue-600'><Link href="/signin">signin</Link></span> </h1>
-        <button className='w-full bg-indigo-600 my-5 text-white py-1 rounded-md' type="submit">Sign Up</button>
+       
+        <h1 className='text-center'>New to Entertainment Website please <span className='text-blue-600'><Link href="/signup">signup</Link></span> </h1>
+        <button className='w-full bg-indigo-600 my-5 text-white py-1 rounded-md' type="submit">Signin</button>
       </form>
     </div>
   );
 };
 
-export default Signup;
+export default Signin;
